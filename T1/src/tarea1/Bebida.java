cd8fe1de219f3c4811863bd691e657e3c29f41f8
Constanza Cristinich <109244120+krsconnie@users.noreply.github.com>
2023-04-28 21:23:32 -0400

    Se crea Clase Producto y sus subclases Bebida y Dulce

---
+++ T1/src/tarea1/Bebida.java
@@ -0,0 +1,22 @@
+package tarea1;
+
+/**
+ *Subclase de Producto,
+ *representa modelo para Bebida
+ */
+public abstract class Bebida extends Producto{
+    private int serie;
+    /**
+     * Constructor de Producto
+     * @param s único int
+     */
+    public Bebida(int s) {
+        super(s);
+    }
+
+    public abstract String consumir();
+    public int getSerie() {
+        int s = serie;
+        return serie;
+    }
+}
